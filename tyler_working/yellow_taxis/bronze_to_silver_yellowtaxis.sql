0f9c18e236ca0b9056bf682180fc61efea84f854
tWoodstockKubrick <tylerwoodstock@kubrickgroup.com>
2024-04-12 17:03:59 +0100

    cleaning bronze to silver scripts uploaded for yellow taxis, currently targetting 2018 bronze database only

---
+++ tyler_working/yellow_taxis/bronze_to_silver_yellowtaxis.sql
@@ -0,0 +1,66 @@
+-- Setup
+USE WAREHOUSE tyler_wh;
+USE DATABASE silver_layer;
+USE SCHEMA test;
+
+-- source table columns:
+SHOW COLUMNS IN bronze_layer.flattened.yellow_flat;
+
+
+-- testing create silver table
+WITH test_silver_cte AS (
+SELECT
+        CASE 
+        WHEN dolocationid < 1 OR dolocationid > 265 THEN 264 -- 264 means unknown in zone mapping
+        ELSE dolocationid END AS 
+    dolocationid,
+        CASE 
+        WHEN pulocationid < 1 OR pulocationid > 265 THEN 264 -- 264 means unknown in zone mapping
+        ELSE pulocationid END AS 
+    pulocationid,
+        CASE 
+        WHEN ratecodeid < 1 OR ratecodeid > 6 THEN 7 -- SET 7 as UNKNOWN FOR RATECODE ID IN DIMENSION TABLE
+        WHEN ratecodeid IS NULL THEN 7
+        ELSE ratecodeid END AS
+    ratecodeid,
+        CASE 
+        WHEN vendorid < 1 OR vendorid > 2 THEN 3 -- SET 3 as UNKNOWN FOR RATECODE ID IN DIMENSION TABLE
+        WHEN vendorid IS NULL THEN 3
+        ELSE vendorid END AS 
+    vendorid,
+        CASE 
+        WHEN extra NOT IN (0, 0.5, 1, 4.5, 2.75, -0.5, -1, -2.75, -4.5) THEN NULL
+        WHEN extra < 0 THEN extra*-1
+        ELSE extra END AS
+    extra,
+        CASE 
+        WHEN fare_amount > 500 OR fare_amount < -500 THEN NULL
+        WHEN fare_amount BETWEEN -500 AND 0 THEN fare_amount*-1
+        ELSE fare_amount END AS
+    fare_amount,
+        CASE
+        WHEN improvement_surcharge NOT IN (-0.3, 0, 0.3) THEN NULL
+        WHEN improvement_surcharge = -0.3 THEN improvement_surcharge*-1
+        ELSE improvement_surcharge END AS 
+    improvement_surcharge,
+        CASE 
+        WHEN mta_tax NOT IN (-0.5, 0, 0.5) THEN NULL
+        WHEN mta_tax = -0.5 THEN mta_tax*-0.5
+        ELSE mta_tax END AS 
+    mta_tax,
+        CASE
+        WHEN passenger_count < 0 OR passenger_count > 6 THEN NULL
+        ELSE passenger_count END AS
+    passenger_count,
+        CASE
+        WHEN payment_type < 1 OR payment_type > 6 THEN 5
+        WHEN payment_type IS NULL THEN 5
+        ELSE payment_type END AS
+    payment_type
+FROM bronze_layer.flattened.yellow_flat
+)  
+SELECT  
+    DISTINCT payment_type
+FROM test_silver_cte;
+
+
