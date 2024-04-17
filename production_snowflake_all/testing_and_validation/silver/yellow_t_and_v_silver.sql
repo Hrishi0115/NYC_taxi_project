5f3d843574018292fd84d133cc6ce305f7d86460
tWoodstockKubrick <tylerwoodstock@kubrickgroup.com>
2024-04-17 15:39:07 +0100

    update tyler

--- production_snowflake_all/testing_and_validation/silver/yellow_t_and_v_silver.sql
+++ production_snowflake_all/testing_and_validation/silver/yellow_t_and_v_silver.sql
@@ -3,13 +3,18 @@
 ---- this script validates correct transformations have been applied
 
 
--- create error table for silver layer yellow taxis
+
+------ create error table for silver layer yellow taxis ------
 DROP TABLE IF EXISTS error_checking.silver.yellow_errors;
 CREATE OR REPLACE TABLE error_checking.silver.yellow_errors
 (
     row_id INT,
     message STRING -- message will change depending on test
 );
+
+
+
+------ populate error table with values that dont conform to silver cleaning/transformations outlined in data dictionary
 
 
 -- 1. dolocationid column
@@ -23,6 +28,15 @@ WHERE dolocationid NOT BETWEEN 1 AND 265
    OR dolocationid IS NULL;
 
 
+-- 2. pulocationid column
+-- should be between 1-265, no nulls
+INSERT INTO error_checking.silver.yellow_errors
+SELECT
+    id,
+    'invalid pulocationid'
+FROM silver_layer.test.yellow
+WHERE pulocationid NOT BETWEEN 1 AND 265
+   OR pulocationid IS NULL;
 
 
 
