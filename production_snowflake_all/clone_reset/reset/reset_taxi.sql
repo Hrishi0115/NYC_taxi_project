e42797881e0a5405fb8fdd76d3b99fc1c079ce1c
tWoodstockKubrick <tylerwoodstock@kubrickgroup.com>
2024-04-18 16:24:30 +0100

    created clones for taxi fact table

---
+++ production_snowflake_all/clone_reset/reset/reset_taxi.sql
@@ -0,0 +1,15 @@
+-- reset fact table to 2018 clone
+
+-- drop fact table
+DROP TABLE IF EXISTS nyc_taxi.gold.fact_taxi_trip;
+
+-- create table as clone
+CREATE TABLE nyc_taxi.gold.fact_taxi_trip AS 
+SELECT * 
+FROM clone.gold.FACT_TAXI_TRIP2018
+;
+
+
+SELECT COUNT(*) FROM nyc_taxi.gold.fact_taxi_trip; --110568004
+
+USE WAREHOUSE tyler_wh;
