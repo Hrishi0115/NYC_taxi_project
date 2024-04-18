3b963d6fd4b02d59b5ee3982863d86cbe052fbc2
Nettaji Babuji <nettajibabuji@kubrickgroup.com>
2024-04-18 12:07:54 +0100

    Making changes to g+y test files

--- production_snowflake_all/testing_and_validation/silver/green_t_and_v_silver.sql
+++ production_snowflake_all/testing_and_validation/silver/green_t_and_v_silver.sql
@@ -18,7 +18,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid dolocationid'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE dolocationid NOT BETWEEN 1 AND 265
    OR dolocationid IS NULL;
 
@@ -32,7 +32,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid pulocationid'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE pulocationid NOT BETWEEN 1 AND 265
    OR pulocationid IS NULL;
 
@@ -46,7 +46,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid ratecodeid'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE ratecodeid NOT IN (1, 2, 3, 4, 5, 6, 7)
    OR ratecodeid IS NULL;
 
@@ -60,7 +60,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid vendorid'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE vendorid NOT IN (1, 2, 3)
    OR vendorid IS NULL;
 
@@ -74,7 +74,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid extra'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE extra NOT IN (0, 0.5, 1, 2.75, 4.5)
    AND extra IS NOT NULL;
 
@@ -88,7 +88,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid fare_amount'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE fare_amount NOT BETWEEN 0 AND 500
    AND fare_amount IS NOT NULL;
 
@@ -102,7 +102,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid improvement_surcharge'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE improvement_surcharge NOT IN (0, 0.3)
    AND improvement_surcharge IS NOT NULL;
 
@@ -116,7 +116,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid mta_tax'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE mta_tax NOT IN (0, 0.5)
    AND mta_tax IS NOT NULL;
 
@@ -131,7 +131,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid passenger_count'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE passenger_count NOT IN (0, 1, 2, 3, 4, 5, 6)
    AND passenger_count IS NOT NULL;
 
@@ -145,7 +145,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid payment_type'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE payment_type NOT IN (1, 2, 3, 4, 5, 6)
    OR payment_type IS NULL;
 
@@ -158,7 +158,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'store_and_fwd_flag'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE store_and_fwd_flag NOT IN ('Y', 'N', 'U')
    OR store_and_fwd_flag IS NULL;
  
@@ -169,7 +169,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid tip_amount'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE tip_amount < 0
     AND tip_amount IS NOT NULL;
  
@@ -180,49 +180,49 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid tolls_amount'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE tolls_amount NOT BETWEEN 0 AND 120
     AND tolls_amount IS NOT NULL;
 
--- 14. lpep_dropoff_date column
+-- 14. tpep_dropoff_date column
 -- should be between 2018-2024, nulls allowed
 INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
-    'invalid lpep_dropoff_date'
-FROM silver_layer.test.green
-WHERE YEAR(lpep_dropoff_date) NOT BETWEEN 2018 AND 2024
-    AND lpep_dropoff_date IS NOT NULL;
+    'invalid tpep_dropoff_date'
+FROM nyc_taxi.silver.green
+WHERE YEAR(tpep_dropoff_date) NOT BETWEEN 2018 AND 2024
+    AND tpep_dropoff_date IS NOT NULL;
 
--- 15. lpep_dropoff_time column
+-- 15. tpep_dropoff_time column
 -- should not be a negative number, nulls allowed
 INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
-    'lpep_dropoff_time'
-FROM silver_layer.test.green
-WHERE lpep_dropoff_time < 0
-    AND lpep_dropoff_time IS NOT NULL;
+    'tpep_dropoff_time'
+FROM nyc_taxi.silver.green
+WHERE tpep_dropoff_time < 0
+    AND tpep_dropoff_time IS NOT NULL;
 
--- 16. lpep_pickup_date column
+-- 16. tpep_pickup_date column
 -- should be between 2018-2024, nulls allowed
 INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
-    'invalid lpep_pickup_date'
-FROM silver_layer.test.green
-WHERE YEAR(lpep_pickup_date) NOT BETWEEN 2018 AND 2024
-    AND lpep_pickup_date IS NOT NULL;
+    'invalid tpep_pickup_date'
+FROM nyc_taxi.silver.green
+WHERE YEAR(tpep_pickup_date) NOT BETWEEN 2018 AND 2024
+    AND tpep_pickup_date IS NOT NULL;
 
--- 17. lpep_pickup_time column
+-- 17. tpep_pickup_time column
 -- should not be a negative number, nulls allowed
 INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
-    'lpep_pickup_time'
-FROM silver_layer.test.green
-WHERE lpep_pickup_time < 0
-    AND lpep_pickup_time IS NOT NULL;
+    'tpep_pickup_time'
+FROM nyc_taxi.silver.green
+WHERE tpep_pickup_time < 0
+    AND tpep_pickup_time IS NOT NULL;
 
 -- 18. trip_distance column
 -- Should be between 0-200, or nulls
@@ -231,7 +231,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid trip_distance'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE trip_distance NOT BETWEEN 0 AND 200
    AND trip_distance IS NOT NULL;
 
@@ -241,7 +241,7 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'total_amount'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE total_amount < 0
     AND total_amount IS NOT NULL;
 
@@ -252,6 +252,6 @@ INSERT INTO error_checking.silver.green_errors
 SELECT
     id,
     'invalid trip_type'
-FROM silver_layer.test.green
+FROM nyc_taxi.silver.green
 WHERE trip_type NOT IN (1, 2, 3)
    OR trip_type IS NULL;
