25183563e4ba159d97e4c4adb34e86d29d8b5879
tWoodstockKubrick <tylerwoodstock@kubrickgroup.com>
2024-04-18 10:28:18 +0100

    updated yellow file to production tables

--- production_snowflake_all/bronze/yellow_load_bronze.sql
+++ production_snowflake_all/bronze/yellow_load_bronze.sql
@@ -1,19 +1,16 @@
--- Setup
 USE WAREHOUSE tyler_big_wh;
-USE DATABASE bronze_layer;
-USE SCHEMA json;
 
 -- azure stage for triathlon azure blob using SAS token
-CREATE OR REPLACE STAGE bronze_layer.json.yellow_2018_onwards_azure_stage
+CREATE OR REPLACE STAGE nyc_taxi.bronze.yellow_2018_onwards_azure_stage
 url = 'azure://triathlonnyc.blob.core.windows.net/output/yellow/'
 credentials = (AZURE_SAS_TOKEN = 'sp=rl&st=2024-04-10T13:48:21Z&se=2024-04-19T21:48:21Z&spr=https&sv=2022-11-02&sr=c&sig=gnfFLDUs19AGWxqa10Hp%2BQctVrHQe6I6DC2s2qhWOns%3D')
 ;
 
 -- inspect inside stage (change end of url to see different files)
-list @yellow_2018_onwards_azure_stage;
+list @nyc_taxi.bronze.yellow_2018_onwards_azure_stage;
 
 -- parquet file format 
-create or replace FILE FORMAT parquet_taxis_yellow
+create or replace FILE FORMAT nyc_taxi.bronze.parquet_taxis_yellow
 TYPE = PARQUET
 ;
 
@@ -25,15 +22,14 @@ TYPE = PARQUET
 -- LIMIT 10;
 
 -- create bronze json table
-CREATE OR REPLACE TABLE bronze_layer.json.yellow_from_2018_in (taxi_col VARIANT);
+CREATE OR REPLACE TABLE nyc_taxi.bronze.yellow (taxi_col VARIANT);
 
 -- copy staged data into snowflake table
-COPY INTO bronze_layer.json.yellow_from_2018_in
-FROM @yellow_2018_onwards_azure_stage
-file_format = parquet_taxis_yellow
+COPY INTO nyc_taxi.bronze.yellow
+FROM @nyc_taxi.bronze.yellow_2018_onwards_azure_stage
+file_format = nyc_taxi.bronze.parquet_taxis_yellow
 pattern = '.*yellow_tripdata_2018.*';
 
 -- inspect inserted data
--- USE WAREHOUSE tyler_wh;
--- SELECT COUNT(*) FROM bronze_layer.json.yellow_from_2018_in;
+SELECT COUNT(*) FROM nyc_taxi.bronze.yellow;
 
