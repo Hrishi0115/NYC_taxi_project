c0058bd5172c3236bbe5378aeb227d113566f3e2
cecilybell <cecilybell@kubrickgroup.com>
2024-04-18 11:37:09 +0100

    testing fhv

--- production_snowflake_all/testing_and_validation/gold/fhv_fact_table_t_and_v_gold.sql
+++ production_snowflake_all/testing_and_validation/gold/fhv_fact_table_t_and_v_gold.sql
@@ -1,15 +1,34 @@
-
+use warehouse cecily_xs;
 -- checking number of rows
 
+-- silver vs gold
 -- fhv
-SELECT COUNT(*) FROM silver_layer.test.fhv; -- 260,874,753
+SELECT COUNT(*) FROM silver.fhv; -- 260,874,753
 SELECT COUNT(*) 
-FROM GOLD_LEVEL.PUBLIC.FHV_TRIP_FACT_TABLE
+FROM GOLD.FACT_FHV_TRIP
 WHERE hvfhs_license_number IS NULL; -- 260,874,753
 
 -- fhv hv
 SELECT COUNT(*) FROM silver_layer.test.fhvhv; -- 60,000,305
 SELECT COUNT(*) 
-FROM GOLD_LEVEL.PUBLIC.FHV_TRIP_FACT_TABLE
+FROM GOLD.FACT_FHV_TRIP
 WHERE hvfhs_license_number IS NOT NULL; -- 60,000,305
+
+
+-- check dimensions
+-- base_dim
+SELECT COUNT(*)
+FROM GOLD.FACT_FHV_TRIP AS fact
+JOIN gold.base_dim AS dim
+ON fact.dispatching_base_number = dim.base_num; -- 260,874,753
+
+SELECT COUNT(*) FROM gold.fact_fhv_trip; -- 260,874,753
+
+-- 
+
+SELECT * FROM GOLD.FACT_FHV_TRIP LIMIT 5;
+
+SELECT COUNT(*) FROM silver_layer.test.fhv;
+SELECT COUNT(*) FROM gold_level.public.fhv_trip_fact_table;
+SELECT * FROM gold_level.public.fhv_trip_fact_table LIMIT 1;
 
--- production_snowflake_all/testing_and_validation/gold/fhv_t_and_v_gold.sql
+++
@@ -1,6 +0,0 @@
-use warehouse cecily_xs;
--- check same number of rows in silver and gold
-SELECT COUNT(*) FROM silver_layer.test.fhv;
-SELECT COUNT(*) FROM gold_level.public.fhv_trip_fact_table;
-SELECT * FROM gold_level.public.fhv_trip_fact_table LIMIT 1;
-
