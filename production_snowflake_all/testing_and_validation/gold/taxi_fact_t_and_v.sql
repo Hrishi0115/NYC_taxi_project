15b40619a168ba7827b062916a40356635af2943
tWoodstockKubrick <tylerwoodstock@kubrickgroup.com>
2024-04-19 10:29:51 +0100

    finished unit test script for taxi_fact table

--- production_snowflake_all/testing_and_validation/gold/taxi_fact_t_and_v.sql
+++ production_snowflake_all/testing_and_validation/gold/taxi_fact_t_and_v.sql
@@ -1,8 +1,15 @@
 
 
--- see 
+------ pre testing  -----
 
--- gold layer error table to log faulty values in taxi fact table
+-- -- see num rows in taxi fact table for test scripts:
+-- SELECT COUNT(*) FROM nyc_taxi.gold.fact_taxi_trip;
+
+-- SHOW COLUMNS IN nyc_taxi.gold.fact_taxi_trip;
+
+
+
+------ create gold layer error table to log faulty values in taxi fact table ------
 DROP TABLE IF EXISTS error_checking.gold.taxi_fact_errors;
 CREATE OR REPLACE TABLE error_checking.gold.taxi_fact_errors
 (
@@ -10,6 +17,8 @@ CREATE OR REPLACE TABLE error_checking.gold.taxi_fact_errors
     message STRING -- message will change depending on test
 );
 
+
+------ run tests ------
 
 -- 1. taxi_colour_id
 -- should be 1, 2 or null
@@ -151,11 +160,108 @@ WHERE payment_type_id NOT IN (1,2,3,4,5,6)
    OR payment_type_id IS NULL;
 
 
+-- 13. fare_amount column
+-- should be between 0 and 500 or null
+INSERT INTO error_checking.gold.taxi_fact_errors
+SELECT
+    taxi_trip_id,
+    'invalid fare_amount'
+FROM nyc_taxi.gold.fact_taxi_trip
+WHERE fare_amount NOT BETWEEN 0 AND 500
+   AND fare_amount IS NOT NULL;
+
+
+-- 14. extra column
+-- should be in (0, 0.5, 1, 2.75, 4.5) or null
+INSERT INTO error_checking.gold.taxi_fact_errors
+SELECT
+    taxi_trip_id,
+    'invalid extra'
+FROM nyc_taxi.gold.fact_taxi_trip
+WHERE extra NOT IN (0, 0.5, 1, 2.75, 4.5)
+   AND extra IS NOT NULL;
+
+
+-- 15. mta_tax column
+-- should be in (0, 0.5) or null
+INSERT INTO error_checking.gold.taxi_fact_errors
+SELECT
+    taxi_trip_id,
+    'invalid mta_tax'
+FROM nyc_taxi.gold.fact_taxi_trip
+WHERE mta_tax NOT IN (0, 0.5)
+   AND mta_tax IS NOT NULL;
+
+
+-- 16. improvement_surcharge column
+-- should be in (0, 0.3) or null
+INSERT INTO error_checking.gold.taxi_fact_errors
+SELECT
+    taxi_trip_id,
+    'invalid improvement_surcharge'
+FROM nyc_taxi.gold.fact_taxi_trip
+WHERE improvement_surcharge NOT IN (0, 0.3)
+   AND improvement_surcharge IS NOT NULL;
+
+
+-- 17. tip_amount column
+-- should not be a negative number, nulls allowed
+INSERT INTO error_checking.gold.taxi_fact_errors
+SELECT
+    taxi_trip_id,
+    'invalid tip_amount'
+FROM nyc_taxi.gold.fact_taxi_trip
+WHERE tip_amount < 0
+    AND tip_amount IS NOT NULL;
+
+
+-- 18. total_amount column
+-- should not be a negative number, nulls allowed
+INSERT INTO error_checking.gold.taxi_fact_errors
+SELECT
+    taxi_trip_id,
+    'invalid total_amount'
+FROM nyc_taxi.gold.fact_taxi_trip
+WHERE total_amount < 0
+    AND total_amount IS NOT NULL;  
+
+
+-- 19. trip_type_id column
+-- Should be in (1,2,3), no nulls
+INSERT INTO error_checking.gold.taxi_fact_errors
+SELECT
+    taxi_trip_id,
+    'invalid trip_type_id'
+FROM nyc_taxi.gold.fact_taxi_trip
+WHERE trip_type_id NOT IN (1,2,3)
+   OR trip_type_id IS NULL;
+
+
+-- 20. airport_fee column
+-- should in (0, 1.25), nulls allowed.
+INSERT INTO error_checking.gold.taxi_fact_errors
+SELECT
+    taxi_trip_id,
+    'invalid airport_fee'
+FROM nyc_taxi.gold.fact_taxi_trip
+WHERE airport_fee NOT IN (0,1.25)
+   AND airport_fee IS NOT NULL;
+
+
+-- 21. congestion_surcharge column
+-- should in (0-120), or nulls.
+INSERT INTO error_checking.gold.taxi_fact_errors
+SELECT
+    taxi_trip_id,
+    'invalid congestion_surcharge'
+FROM nyc_taxi.gold.fact_taxi_trip
+WHERE congestion_surcharge BETWEEN 0 AND 120
+   AND congestion_surcharge IS NOT NULL;
 
 
 ------ checking results of tests ------
--- error table overview after all tests:
-SELECT COUNT(*) FROM error_checking.gold.taxi_fact_errors;
+-- -- error table overview after all tests:
+SELECT COUNT(*) AS bad_rows_in_fact_table FROM error_checking.gold.taxi_fact_errors;
 
 
 -- -- see which columns have rows with errors:
@@ -164,15 +270,16 @@ SELECT COUNT(*) FROM error_checking.gold.taxi_fact_errors;
 -- GROUP BY message
 -- ORDER BY COUNT(*) DESC;
 
+
 -- -- view the actual rows with errors in original table (dates included for full view):
-SELECT
-    gold.*,
-    YEAR(dates.date)
-FROM nyc_taxi.gold.fact_taxi_trip AS gold
-LEFT JOIN nyc_taxi.gold.date_dim AS dates
-    ON gold.pu_date_id = dates.date_id
-INNER JOIN error_checking.gold.taxi_fact_errors AS errors
-    ON gold.taxi_trip_id = errors.row_id; 
+-- SELECT
+--     gold.*,
+--     YEAR(dates.date)
+-- FROM nyc_taxi.gold.fact_taxi_trip AS gold
+-- LEFT JOIN nyc_taxi.gold.date_dim AS dates
+--     ON gold.pu_date_id = dates.date_id
+-- INNER JOIN error_checking.gold.taxi_fact_errors AS errors
+--     ON gold.taxi_trip_id = errors.row_id; 
 
 
 
@@ -185,29 +292,3 @@ INNER JOIN error_checking.gold.taxi_fact_errors AS errors
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- -- standalone test: check all rows have been added (taxi_fact rows should be = yellow_silver rows + bronze_silver rows)
--- SELECT
---     COUNT(*) AS rows_in_fact_table,
---     (SELECT COUNT(*) FROM nyc_taxi.silver.yellow) AS rows_in_silver_yellow,
---     (SELECT COUNT(*) FROM nyc_taxi.silver.green) AS rows_in_silver_green,
---     (SELECT COUNT(*) FROM nyc_taxi.silver.yellow) + (SELECT COUNT(*) FROM nyc_taxi.silver.green) AS silver_plus_green,
---     COUNT(*) - ((SELECT COUNT(*) FROM nyc_taxi.silver.yellow) + (SELECT COUNT(*) FROM nyc_taxi.silver.green)) AS lost_rows_between_silver_and_gold,
--- FROM nyc_taxi.gold.fact_taxi_trip;
